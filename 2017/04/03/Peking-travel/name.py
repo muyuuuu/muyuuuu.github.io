35d5b54f22c43e82dfa2eddab2d3f4ff989258fe
muyuuuu <muyu0223@outlook.com>
2020-02-02 18:18:41 +0800

    Site updated: 2020-02-02 18:18:34

--- 2017/04/03/Peking-travel/name.py
+++
@@ -1,9 +0,0 @@
-import os
-
-path_name='E:/Hexo/hexokit/source/_posts/Peking-travel'
-#path_name :表示你需要批量改的文件夹
-i=0
-for item in os.listdir(path_name):#进入到文件夹内，对每个文件进行循环遍历
-    os.rename(os.path.join(path_name,item),os.path.join(path_name,(str(i)+'.jpg')))
-    #os.path.join(path_name,item)表示找到每个文件的绝对路径并进行拼接操作
-    i+=1
